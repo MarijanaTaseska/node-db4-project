6f429253f9386a5904bea85073ed8cf7da69ab08
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-14 23:53:06 -0700

    getRecipeBy ID

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,11 @@
-function getRecepieById(recipe_id) {
-    return Promise.resolve(`awesome recipe with id ${recipe_id}`)
+const db = require('../../data/db_config')
+    
+async function getRecepieById(recipe_id) {
+const recepieRows = await db('recipes as r')
+.where('recipe_id', recipe_id)
+
+return recepieRows
+
 }
 
 module.exports = { getRecepieById }
