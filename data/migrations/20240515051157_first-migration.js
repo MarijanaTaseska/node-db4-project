6d8498ec774ac0b3928ff6d9e6c4aa88ebfad88f
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-14 22:22:59 -0700

    ingredients table created

--- data/migrations/20240515051157_first-migration.js
+++ data/migrations/20240515051157_first-migration.js
@@ -4,11 +4,14 @@
  */
 exports.up =async function(knex) {
   await knex.schema
-  .createTable('recepis', table => {
-    table.increments()
+  .createTable('recipes', table => {
+    table.increments('recipe_id')
+    table.string('recipe_name', 200).notNullable().unique()
   })
   .createTable('ingredients', table => {
-    table.increments()
+    table.increments('ingredient_id')
+    table.string('ingredient_name', 200).notNullable().unique()
+    table.string('ingredient_unit', 50)
   })
   .createTable('steps', table => {
     table.increments()
